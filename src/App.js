cb04e505bb54760ca8543fde9adc207ef597ad43
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-01-18 11:33:41 +0530

    one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
+import { app } from './firebaseconfig';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <h1 className="text-3xl font-bold underline">
+        <Navbar />
+        Hello world!
+      </h1>
     </div>
   );
 }
