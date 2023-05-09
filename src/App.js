bb765c631ad2c3744138a36881299e769a538c96
iamabhaytiwari343 <108920327+iamabhaytiwari343@users.noreply.github.com>
2023-05-09 08:48:30 +0530

    working on search components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+import Search from "./components/Search.jsx"
 import './App.css';
 
 function App() {
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
   return (
-    <div className="App">
-      Search
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
     </div>
   );
 }
